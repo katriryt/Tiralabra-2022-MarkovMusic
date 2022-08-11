a034707d4a8c3bcb8b1751716b2f1bbc47621868
Katri <katri.rytkonen@outlook.com>
2022-08-11 09:51:43 +0300

    Word frequencies added to dictionary and trie

--- src/repositories/db_utilities.py
+++ src/repositories/db_utilities.py
@@ -1,16 +1,21 @@
 from pathlib import Path
+import json
 from repositories.trie import trie as default_trie
 
 
 class EnglishDictionary:
     """Class populates the spellchecker's trie data structure with
-        English words.
+        English words and their frequencies.
     """
 
     def __init__(self, trie=default_trie):
         #        print("initializing dictionary")
         self.trie = trie
-#        self._populate_trie_with_words()
+        self.words_in_original_dictionary = 0
+        self.frequencies_in_original_dictionary = 0
+        self.words_in_trie = 0
+        self.frequencies_in_trie = 0
+
         self.file_name = self.get_english_word_file_location()
         self._populate_trie_based_on_file()
 
@@ -19,21 +24,29 @@ class EnglishDictionary:
         """
 #        print("getting file location")
         script_location = Path(__file__).absolute().parent.parent.parent
-        file_location = script_location/"data"/"english_words.csv"
+        file_location = script_location/"data"/"english_word_frequency_dictionary.json"
         return file_location
 
     def _populate_trie_based_on_file(self):
-        """Method populates trie with the words from English dictionary that
-        only contain English alphabets e.g. words with numbers are excluded.
+        """Method populates trie with the words and their frequencies from
+        English dictionary that only contain English alphabets
+        e.g. words with numbers are excluded.
         """
-        #        print("getting words")
-        with open(self.file_name) as given_file:
-            contents = given_file.readlines()
-            # This row may be limited during testing [600:900]
-            for row in contents:
-                if row.strip().isalpha() is True:
-                    self.trie.insert_nodes(row.strip())
-#        print("words saved")
+        words_frequencies_dictionary = json.load(open(self.file_name, 'r'))
+
+        for key, value in words_frequencies_dictionary.items():
+        #    print(f"{key}: {value}")
+            self.words_in_original_dictionary += 1
+            self.frequencies_in_original_dictionary += value
+            word = str(key)
+            if word.isalpha() is True:
+                self.trie.insert_nodes(word, value)
+                self.words_in_trie += 1
+                self.frequencies_in_trie += value
+
+        print(f"""original dictionary. Words: {self.words_in_original_dictionary}
+                and frequencies: {self.frequencies_in_original_dictionary}""")
+        print(f"trie. Words: {self.words_in_trie} and frequencies: {self.frequencies_in_trie}")
 
 
 english_dictionary = EnglishDictionary()
